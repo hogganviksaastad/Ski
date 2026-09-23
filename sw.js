fd9666b16b585bed7b8ac37b4c71daf881cbf21d
hogganviksaastad <hogganviksaastad@gmail.com>
2026-09-23 09:05:48 +0200

    Lengde valg pri

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// ski-selector-redesign-1 service worker
-const CACHE_NAME = "ski-selector-redesign-1";
+// ski-selector-length-fix-1 service worker
+const CACHE_NAME = "ski-selector-length-fix-1";
 const CORE = [
   "./",
   "./index.html",
