0efecb673bc676f4cd15ffa31501e3fd471500cc
hogganviksaastad <hogganviksaastad@gmail.com>
2026-09-23 11:42:35 +0200

    ikon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// ski-selector-length-fix-1 service worker
-const CACHE_NAME = "ski-selector-length-fix-1";
+// ski-selector-new-icon-1 service worker
+const CACHE_NAME = "ski-selector-new-icon-1";
 const CORE = [
   "./",
   "./index.html",
